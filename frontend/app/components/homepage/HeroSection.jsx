f83e97341396c304c49251c99b72965fec9c3792
Jan <kupper.thorge@gmail.com>
2025-04-10 13:26:06 +0200

    Added hero section

--- frontend/app/components/homepage/HeroSection.jsx
+++ frontend/app/components/homepage/HeroSection.jsx
@@ -1,7 +1,18 @@
 import React from "react";
 
 const HeroSection = () => {
-  return <div>HeroSection</div>;
+  return (
+    <div className="hero card bg-accent">
+      <div className="hero-content max-w-lg flex-col py-12 text-center">
+        <h1>Förenkla din väg till arbetshjälpmedel</h1>
+        <p className="py-2 !text-base">
+          Låt vår AI-assistent guida dig genom ansökningsprocessen och maximera dina chanser att få
+          rätt stöd för dina behov.
+        </p>
+        <button className="btn btn-primary">Kom igång nu</button>
+      </div>
+    </div>
+  );
 };
 
 export default HeroSection;
