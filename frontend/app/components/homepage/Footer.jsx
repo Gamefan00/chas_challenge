9754726c53b700291216dddd29958d1dcd1bd6be
Your Name <you@example.com>
2025-04-10 14:40:36 +0200

    Implement footer component with navigation links and contact information

--- frontend/app/components/homepage/Footer.jsx
+++ frontend/app/components/homepage/Footer.jsx
@@ -1,7 +1,46 @@
 import React from "react";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className="bg-footer text-background w-full flex-col">
+      <div className="flex justify-between gap-10 p-10">
+        <div className="flex flex-col items-start gap-2">
+          <h2>Ansökshjälpen</h2>
+          <div className="bg-primary h-[2px] w-2/12" />
+          <div className="flex flex-col gap-4 opacity-90">
+            <a href="">Om oss</a>
+            <a href="">Tillgänglighetsredogörelse</a>
+            <a href="">Användervillkor</a>
+            <a href="">Integritespolicy</a>
+          </div>
+        </div>
+        <div className="flex flex-col items-start gap-2">
+          <h2>Resurser</h2>
+          <div className="bg-primary h-[2px] w-2/12" />
+          <div className="flex flex-col gap-4 opacity-90">
+            <a href="">Lager & regler</a>
+            <a href="">Försäkringskassan</a>
+            <a href="">Funktionsrättsorganisationer</a>
+            <a href="">Vanliga hjälpmedel</a>
+          </div>
+        </div>
+        <div className="flex flex-col items-start gap-2">
+          <h2>Kontakt</h2>
+          <div className="bg-primary h-[2px] w-2/12" />
+          <div className="flex flex-col gap-4 opacity-90">
+            <a href="">inte@ansokningshalpen.se</a>
+            <a href="">070-123 45 67</a>
+            <a href="">Kontaktformulär</a>
+            <a href="">Feedback</a>
+          </div>
+        </div>
+      </div>
+      <div className="bg-base-100 h-px w-full opacity-20" />
+      <div className="flex justify-center p-4 text-sm">
+        <h4 className="">© 2025 Ansökshjälpen. Alla rättigheter förbehållna</h4>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
