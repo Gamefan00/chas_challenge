81abe1172846247a7099bbf629b9da84931fa354
Jan <kupper.thorge@gmail.com>
2025-04-12 16:04:46 +0200

    set up basic express backend

---
+++ backend/server.js
@@ -0,0 +1,25 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import OpenAI from 'openai';
+
+// Load .env file before server starts, needed for dev environment
+import dotenv from 'dotenv';
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT;
+
+// Middleware
+app.use(bodyParser.json());
+app.use(cors());
+
+// Initialize OpenAI
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+// Start server
+app.listen(PORT, () => {
+  console.log('Started on port: ' + PORT);
+});
