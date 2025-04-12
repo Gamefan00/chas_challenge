54a90d6ca25028a83ff106b68a27e48b7b5ddbe9
Jan <kupper.thorge@gmail.com>
2025-04-12 15:21:41 +0200

    added basic chat functionality

--- frontend/app/chatbot/page.js
+++ frontend/app/chatbot/page.js
@@ -1,14 +1,30 @@
 "use client";
 
-import { useState } from "react";
+import { Loader2 } from "lucide-react";
+import { useEffect, useState } from "react";
 import Markdown from "react-markdown";
 
 export default function ChatBot() {
   const BASE_URL = "localhost";
   const [prompt, setPrompt] = useState("");
-  const [outputText, setOutputText] = useState("Output here.");
+  const [isLoading, setIsLoading] = useState(false);
+  const [messages, setMessages] = useState(undefined);
 
+  // Load existing messages on pageload, should be done through localstorage maybe?
+  useEffect(() => {
+    async function getMessages() {
+      const response = await fetch(`http://${BASE_URL}:4000/get_messages`, {
+        method: "GET",
+      });
+      const data = await response.json();
+      setMessages(data.data);
+    }
+    getMessages();
+  }, []);
+
+  // Send user prompt to the assistant api and await response
   async function sendPrompt() {
+    setIsLoading(true);
     console.log(`Sending prompt...\nPrompt: ${prompt}`);
 
     const response = await fetch(`http://${BASE_URL}:4000/output_text`, {
@@ -18,17 +34,32 @@ export default function ChatBot() {
     });
 
     const data = await response.json();
-
     console.log("Response from server:", data);
-
-    setOutputText(data.output_text);
+    setMessages(data.data);
     setPrompt("");
+    setIsLoading(false);
   }
 
   return (
     <div className="bg-background flex w-full flex-col px-4 py-12 md:pb-20">
       <div className="mx-auto flex min-h-screen w-full max-w-7xl flex-col items-center gap-12 md:gap-20">
         <div className="flex w-full flex-col items-center gap-4">
+          {/* Render messages */}
+          <div className="flex w-full max-w-4xl flex-col gap-4 !text-sm">
+            {messages &&
+              [...messages].reverse().map((message) => (
+                <div
+                  key={message.id}
+                  className={`${message.role === "user" && "ml-auto max-w-1/2 rounded-lg bg-slate-200"} w-full p-2`}
+                >
+                  <div className="markdown-body">
+                    <Markdown>{message.content[0]?.text?.value}</Markdown>
+                  </div>
+                </div>
+              ))}
+          </div>
+
+          {/* User input area */}
           <textarea
             onChange={(e) => setPrompt(e.target.value)}
             className="textarea w-full max-w-md"
@@ -36,21 +67,8 @@ export default function ChatBot() {
             value={prompt}
           ></textarea>
           <button onClick={sendPrompt} className="btn btn-primary">
-            Send
+            {isLoading ? <Loader2 className="animate-spin" /> : "Send"}
           </button>
-          <div className="flex flex-col gap-4 !text-sm">
-            <Markdown
-              components={{
-                // Force p elements to have foreground color
-                p(props) {
-                  const { node, ...rest } = props;
-                  return <p style={{ color: "var(--color-foreground)" }} {...rest} />;
-                },
-              }}
-            >
-              {outputText}
-            </Markdown>
-          </div>
         </div>
       </div>
     </div>
