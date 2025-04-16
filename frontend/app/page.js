44ee78c25eb8a0886a5f44d5a8e020eb1ed726d2
Zarha Buske <zarhabuske@hotmail.com>
2025-04-16 14:02:13 +0200

    fix: refine layout structure in page component

--- frontend/app/page.js
+++ frontend/app/page.js
@@ -6,8 +6,8 @@ import FAQ from "../components/homepage/FAQ";
 
 const page = () => {
   return (
-    <div className="bg-background flex w-full flex-col px-4 py-12 md:pb-20">
-      <div className="mx-auto flex min-h-screen max-w-7xl flex-col items-center gap-12 md:gap-20">
+    <div className="bg-background flex min-h-screen w-full flex-col px-4 py-12 md:pb-20">
+      <div className="mx-auto flex max-w-7xl flex-col items-center gap-12 md:gap-20">
         <HeroSection />
         <FeatureCard />
         <HowItWorks />
