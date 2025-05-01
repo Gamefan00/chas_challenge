5e252b933a55780d33e82b85440c3cb1438ae7b2
Zarha <zarhaselene@gmail.com>
2025-05-01 10:32:46 +0200

    feat: refactor Navbar to use separate Logo component

--- frontend/components/homepage/Navbar.jsx
+++ frontend/components/homepage/Navbar.jsx
@@ -1,10 +1,9 @@
 "use client";
-import { Menu, Plus } from "lucide-react";
+import { Menu } from "lucide-react";
 import Link from "next/link";
-import React from "react";
 import { Button } from "@/components/ui/button";
 import { Sheet, SheetClose, SheetContent, SheetFooter, SheetTrigger } from "../ui/sheet";
-
+import Logo from "../shared/Logo";
 // Data for the links in the Navbar. Add, remove or change links here
 const linksData = [
   { title: "Hem", href: "/" },
@@ -16,13 +15,7 @@ const linksData = [
 const Navbar = () => {
   return (
     <nav className="relative z-50 flex h-16 items-center justify-between px-4 shadow-sm">
-      {/* Logo and site name  */}
-      <div className="items-center">
-        <Link href="/" className="flex items-center">
-          <Plus className="text-background bg-primary mr-2 rounded-md" />
-          <h3 className="text-primary">Ansökshjälpen</h3>
-        </Link>
-      </div>
+      <Logo />
 
       {/* Desktop Navbar */}
       <ul className="hidden items-center gap-6 md:flex">
