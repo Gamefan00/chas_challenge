44fabc94a25069d325366d4165dd02d58e39506f
Zarha <zarhaselene@gmail.com>
2025-05-01 10:34:35 +0200

    move main site into seprate folder

---
+++ frontend/app/(site)/page.js
@@ -0,0 +1,20 @@
+"use client";
+import HeroSection from "../../components/homepage/HeroSection";
+import FeatureCard from "../../components/homepage/FeatureCard";
+import HowItWorks from "../../components/homepage/HowItWorks";
+import FAQ from "../../components/homepage/FAQ";
+
+const page = () => {
+  return (
+    <div className="bg-background flex min-h-screen w-full flex-col px-4 py-12 md:pb-20">
+      <div className="mx-auto flex max-w-7xl flex-col items-center gap-12 md:gap-20">
+        <HeroSection />
+        <FeatureCard />
+        <HowItWorks />
+        <FAQ />
+      </div>
+    </div>
+  );
+};
+
+export default page;
