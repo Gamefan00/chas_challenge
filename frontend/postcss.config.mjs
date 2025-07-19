f9f49df1962996e6150da4c884a3aac676d19a9c
Axel F <nah.forsberg@gmail.com>
2025-07-19 17:03:33 +0200

    Reverting to old changes

--- frontend/postcss.config.mjs
+++ frontend/postcss.config.mjs
@@ -1,6 +1,13 @@
 const config = {
   plugins: {
-    "@tailwindcss/postcss": {},
+    tailwindcss: {},
+    autoprefixer: {},
   },
 };
 export default config;
+
+//const config = {  plugins: {
+//    "@tailwindcss/postcss": {},
+//  },
+//};
+//
